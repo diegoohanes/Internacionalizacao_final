c869921a058121b39b9630d36d00bb68749dfd60
Diego Ohanes <Ohanes@MacBook-Pro-2013.local>
2019-03-10 21:42:27 -0300

    Initial commit

---
+++ app/src/androidTest/java/br/com/diegoohanesrm79740/exinternacionlizacao/ExampleInstrumentedTest.java
@@ -0,0 +1,22 @@
+package br.com.diegoohanesrm79740.exinternacionlizacao;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+
+    }
+}
